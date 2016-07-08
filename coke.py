c4f1ad7000d9b938059a6f3b2aaad3c5010b9cf2
Chris Nowlin <cnowlin@gmail.com>
2016-07-07 17:09:01 -1000

    Adding file coke.py

---
+++ coke.py
@@ -0,0 +1,11 @@
+##########
+# Just writing a bit of Python code to make sure I have everything running to my satisfaction on this new macbook. Github working, etc.
+##########
+
+song = """The sun will always shine.
+The birds will always sing.
+As long as there is thirst,
+There's always the real thing.
+"""
+
+print(song)
