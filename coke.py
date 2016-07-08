691cb4b83927d90ac1abd3e8d7a995e5aa769bb3
Chris Nowlin <cnowlin@gmail.com>
2016-07-07 20:35:34 -1000

    Will now mess with coke.py on the coke branch

--- coke.py
+++ coke.py
@@ -1,34 +1,46 @@
 #!/usr/bin/env python3
 
-##########
-# Just writing a bit of Python code to make sure I have
-# my satisfaction on this new macbook. Github working, etc.
-##########
+##########################################################
+# Test code. We should use it to practice collaborating. #
+# And to develop a baseline of coding standards.         #
+##########################################################
+
 import sys
 import logging
 import argparse
 
-FORMAT = '%(levelname)|%(asctime)-15s| %(message)s'
+FORMAT = '%(levelname)s|%(asctime)-15s| %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.INFO)
 
-
-def main():
-
-    logging.info("BEGIN: coke.py")
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("sing", action='store_true')
-    args = parser.parse_args()
-
+def sing(out):
+    """
+    Writes a song to the specified output
+    """
     song = """The sun will always shine.
 The birds will always sing.
 As long as there is thirst,
 There's always the real thing.
 """
+    out.write(song)
+
+
+
+def main():
+
+    logging.info("BEGIN: coke.py")
+    description = """A toy program, but one to test things out on.
+Can also perhaps serve as a baseline for coding standards."""
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("--sing", action='store_true',
+                        help="A flag to tell the code to sing.")
+    parser.add_argument("--out", type=argparse.FileType('w'),
+                        default=sys.stdout,
+                        help="Output file. Defaults to stdout")
+    args = parser.parse_args()
 
     if args.sing:
         logging.info("Singing")
-        print(song)
+        sing(args.out)
     else:
         logging.info("Not singing")
 
