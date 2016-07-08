c81d29f6186e1f21a4100f31e1af9810e8a9472b
chris-nowlin <cnowlin@gmail.com>
2016-07-07 18:21:19 -1000

    Update coke.py

--- coke.py
+++ coke.py
@@ -1,12 +1,20 @@
+import sys, logging, argparse
 ##########
 # Just writing a bit of Python code to make sure I have
 # my satisfaction on this new macbook. Github working, etc.
 ##########
 
-song = """The sun will always shine.
+
+def main():
+  
+  song = """The sun will always shine.
 The birds will always sing.
 As long as there is thirst,
 There's always the real thing.
 """
 
-print(song)
+
+  print(song)
+
+if __name__ == '__main__':
+  main()
